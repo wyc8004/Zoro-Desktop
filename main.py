15f183ca58f51f7baad5d4e97e042ef29830b442
wyc8004 <95153241+wyc8004@users.noreply.github.com>
2024-09-13 07:12:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+print('Now program is loading')
 import ctypes
 import os
+import zipfile
+import shutil
+def unzip_file(zip_path,extract_to):
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+zip_file_path = os.getcwd()+os.sep+'zoro.zip'
+try:
+    shutil.rmtree(os.getcwd()+os.sep+'zoro')
+    os.mkdir(os.getcwd()+os.sep+'zoro')
+except:
+    pass
+extract_to_path = os.getcwd()
+unzip_file(zip_file_path, extract_to_path)
 def cw(image_path):
     SPI_SETDESKWALLPAPER = 20
     ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER,
@@ -14,7 +28,8 @@ class Zoro(object):
         self.count+=1
         if self.count==2410:
             self.count=59
-if __name__=='__main__':
-    cls=Zoro()
-    while True:
-        cls.mainloop()
+input('Press enter to start run.')
+cls=Zoro()
+while True:
+    cls.mainloop()
+            
